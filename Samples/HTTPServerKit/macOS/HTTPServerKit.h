56b62265f4c5990e0a90df14b75a1fa2e6bc3c0e
Martin Hering <hering@vemedio.com>
2016-06-21 09:57:37 +0200

    fixed tvOS and macOS targets

---
+++ Samples/HTTPServerKit/macOS/HTTPServerKit.h
@@ -0,0 +1,30 @@
+//
+//  HTTPServerKit.h
+//  HTTPServerKit
+//
+//  Created by Martin Hering on 21.06.16.
+//  Copyright © 2016 Vemedio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HTTPServerKit.
+FOUNDATION_EXPORT double HTTPServerKitVersionNumber;
+
+//! Project version string for HTTPServerKit.
+FOUNDATION_EXPORT const unsigned char HTTPServerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPServerKit/PublicHeader.h>
+
+
+#import <HTTPServerKit/HTTPConnection.h>
+#import <HTTPServerKit/HTTPMessage.h>
+#import <HTTPServerKit/HTTPResponse.h>
+#import <HTTPServerKit/HTTPServer.h>
+#import <HTTPServerKit/HTTPAuthenticationRequest.h>
+#import <HTTPServerKit/HTTPAsyncFileResponse.h>
+#import <HTTPServerKit/HTTPDataResponse.h>
+#import <HTTPServerKit/HTTPDynamicFileResponse.h>
+#import <HTTPServerKit/HTTPErrorResponse.h>
+#import <HTTPServerKit/HTTPFileResponse.h>
+#import <HTTPServerKit/HTTPRedirectResponse.h>
